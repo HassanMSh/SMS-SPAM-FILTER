12b356769320d0d6bea3fbe417ab26edc75265ec
Hassan Shamseddine <hassan.m.shamseddine@gmail.com>
2023-01-07 01:05:57 +0200

    Delete CleanDataTwo.py

--- CleanDataTwo.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Datasets/NewData.csv')
-df.drop(['x'], inplace=True, axis=1)
-df.dropna(inplace=True)
-
-df = df[df['SMStext'].map(lambda x: x.isascii())]
-
-df.to_csv('Datasets/NewDataASCII.csv')
